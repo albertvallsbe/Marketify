31f32b65723fa8823bc7a05a601f0f6b03656c0a
David <hernandez.larrea.david@alumnat.copernic.cat>
2023-03-02 20:57:55 +0100

    Created Layout: Main

--- site/resources/views/layouts/main.blade.php
+++ site/resources/views/layouts/main.blade.php
@@ -5,14 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    {{-- <link rel="stylesheet" href="{{asset('css/w3.css')}}"> --}}
-    <title>Laravel App('title')</title>
+    <link rel="stylesheet" href="..\..\css\app.css">
+    <title>@yield('title')</title>
 </head>
 
 <body>
-    <nav>
-        <a  href="{{route('home.index')}}">Laravel App</a>
-    </nav>
+    @yield('content')
 </body>
 
 </html>
