d212a0893cb633e16e909d92904d9b169be384f7
Albert Valls <avalls89@gmail.com>
2023-03-13 20:00:57 +0100

    Add first test to routes

---
+++ site/tests/Feature/RouteTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RouteTest extends TestCase
+{
+    /**
+     * Test route.
+     */
+
+     use RefreshDatabase;
+
+    public function testRouteHome(): void
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+
+    public function testRouteLogin(): void
+    {
+        $response = $this->get('/login');
+        $response->assertSeeText('Log In');
+        $response->assertStatus(200);
+    }
+
+    public function testRouteCart(): void
+    {
+        $response = $this->get('/cart');
+        $response->assertSeeText('Cart');
+        $response->assertStatus(200);
+    }
+}
