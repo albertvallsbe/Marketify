cd722052c14aa22edb28467f40493075f0820170
Albert Valls <avalls89@gmail.com>
2023-03-22 21:25:37 +0100

    Update global styles

--- site/resources/views/components/footer.blade.php
+++ site/resources/views/components/footer.blade.php
@@ -1,9 +1,9 @@
 <footer>
-    <div class="main-container">
-        <h3 class="names-title">David Hernández, Albert Valls & Oscar Ramirez</h3>
-        <div class="container">
-            <img class="logo" src="{{ asset('images/marketify_logo.png') }}">
-        </div>
-        <h3 class="course-title">Course : 2022-2023</h3>
+  <div class="footer">
+    <h3 class="footer__components">David Hernández, Albert Valls & Oscar Ramirez</h3>
+    <div class="footer__logo">
+      <img class="logo" src="{{ asset('images/marketify_logo.png') }}">
     </div>
+    <h3 class="footer__course">Course : 2022-2023</h3>
+  </div>
 </footer>
