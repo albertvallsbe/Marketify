04f9218c528e8d3ac222bb8977bb62761002959f
Albert Valls <avalls89@gmail.com>
2023-04-02 19:58:02 +0200

    Update home cards style

--- site/resources/views/product/index.blade.php
+++ site/resources/views/product/index.blade.php
@@ -3,33 +3,33 @@
 @section('title', 'Marketify')
 
 @section('content')
-    <h1>Products</h1>
-    <div id="products">
-        @if ($products->count())
-            @foreach ($products as $key => $product)
-                <div class="product" id={{ $product->id }}>
-                    <a href="{{ route('product.show', $product->id) }}">
-                        <div class="product__div_img">
-                            <img class="product__img" src="{{ $product->image }}" />
-                        </div>
-                        <h4 class="product__name">{{ $product->name }}</h4>
-                        <h4 class="product__name">{{ $product->tag }}</h4>
-                        <h3 class="product__price">{{ $product->price }} €</h3>
-                    </a>
-                    <button class="btn-cart small_button" data-product-id="{{ $product->id }}">Add to cart</button>
-                </div>
-            @endforeach
-        @else
-            @if (session('request_search') != null)
-                <h2>No results were found with '<?= session('request_search') ?>'.</h2>
-            @else
-                <h2>No results were found.</h2>
-            @endif
-        @endif
-    </div>
-    <div id="pagination">
-        {{-- {{ $products->appends(['search' => $search]) }} --}}
-        {{ $products->links('vendor.pagination.default') }}
-    </div>
-    <script type ="module" src="{{ asset('js/pages/product_index.js') }}"></script>
+  <h1 class="main_title">Products</h1>
+  <div id="products" class="products">
+    @if ($products->count())
+      @foreach ($products as $key => $product)
+        <div class="card-style product" id={{ $product->id }}>
+          <a class="card-style_a" href="{{ route('product.show', $product->id) }}">
+            <div class="product__div_img">
+              <img class="product__img" src="{{ $product->image }}" />
+            </div>
+            <h4 class="card-style_title product__name">{{ $product->name }}</h4>
+            <h4 class="card-style_title product__name">{{ $product->tag }}</h4>
+            <h3 class="card-style_title product__price">{{ $product->price }} €</h3>
+          </a>
+          <button class="btn-cart small_button" data-product-id="{{ $product->id }}">Add to cart</button>
+        </div>
+      @endforeach
+    @else
+      @if (session('request_search') != null)
+        <h2>No results were found with '<?= session('request_search') ?>'.</h2>
+      @else
+        <h2>No results were found.</h2>
+      @endif
+    @endif
+  </div>
+  <div id="pagination">
+    {{-- {{ $products->appends(['search' => $search]) }} --}}
+    {{ $products->links('vendor.pagination.default') }}
+  </div>
+  <script type ="module" src="{{ asset('js/pages/product_index.js') }}"></script>
 @endsection
