9fcede706917502f8a99abb106c0c729961a6c2f
Albert Valls <avalls89@gmail.com>
2023-04-02 15:31:22 +0200

    Update style card to login and register with mixins and refactoring code

--- site/resources/views/login/index.blade.php
+++ site/resources/views/login/index.blade.php
@@ -4,39 +4,37 @@
 
 @section('content')
   <section class="main-container">
-    <section class="container-login">
-      <section class="card_style">
-        <h1 class="login-title">Login</h1>
-        <form method='POST'action="{{ route('auth.login') }}">
-          @csrf
-          <input type="email" class="input-mail" placeholder="example@gmail.com" name="email">
-          <input type="text" class="input-mail" placeholder="_.username._" name="name">
-          @if ($errors->has('login-email'))
-            <span><strong>{{ $errors->first('login-email') }}</strong></span>
-          @endif
-          <input type="password" class="input-password" placeholder="••••••••••••" name="password">
-          @if ($errors->has('login-password'))
-            <span><strong>{{ $errors->first('login-password') }}</strong></span>
-          @endif
-          @if(session()->has('status'))
-            <p class="text-login"> {{ session()->get('status') }} </p>
-          @endif
-          <button class="btn-login general_button">LOG IN</button>
-
-          <p class="text-login users-link">Do you not have an account? <a href="{{ route('register.index') }}">Register</a>
-          </p>
-          <p class="text users-link">Do you forgot the password? <a href="{{(route('login.password')) }}">Remember it.</a>
-          </p>
-
-        </form>
-      </section>
+    <section class="card-style">
+      <h1 class="card-style_title login-title">Login</h1>
+      <form class="form" method='POST'action="{{ route('auth.login') }}">
+        @csrf
+        <input type="email" class="form_input input-mail" placeholder="example@gmail.com" name="email">
+        <input type="text" class="form_input input-mail" placeholder="_.username._" name="name">
+        @if ($errors->has('login-email'))
+          <span>{{ $errors->first('login-email') }}</span>
+        @endif
+        <input type="password" class="form_input input-password" placeholder="••••••••••••" name="password">
+        @if ($errors->has('login-password'))
+          <span>{{ $errors->first('login-password') }}</span>
+        @endif
+        @if(session()->has('status'))
+          <p class="text-login"> {{ session()->get('status') }} </p>
+        @endif
+        <button class="btn-login general_button">LOG IN</button>
+      </form>
+      <p class="users-link text-login">Do you not have an account?
+        <a href="{{ route('register.index') }}">Register</a>
+      </p>
+      <p class="users-link text">Do you forgot the password?
+        <a href="{{(route('login.password')) }}">Remember it.</a>
+      </p>
     </section>
-    <section class="login-text">
+    <section class="login-text intro-style">
         <a href="{{ route('product.index') }}">
-            <img class="img-logo" src="{{ asset('images/marketify_logo.png') }}">
+            <img class="intro-style_logo" src="{{ asset('images/marketify_logo.png') }}">
         </a>
-        <h1 class="login-title">LOG IN TO ENJOY OUR APP!</h1>
-        <p class="text-login">This is an application that allows you to buy products and sell them at the same
+        <h1 class="intro-style_title">LOG IN TO ENJOY OUR APP!</h1>
+        <p class="intro-style_text">This is an application that allows you to buy products and sell them at the same
             time. In this way you can be a buyer and a seller at the same time.<br><br>By: Albert Valls, David Hernández
             & Oscar Ramírez.</p>
     </section>
--- site/tests/Feature/Http/Controller/ProductControllerTest.php
+++ site/tests/Feature/Http/Controller/ProductControllerTest.php
@@ -8,33 +8,33 @@ use Tests\TestCase;
 
 class ProductControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    use RefreshDatabase;
+  /**
+   * A basic feature test example.
+   */
+  use RefreshDatabase;
 
-    public function testNoProducts(): void
-    {
-        $response = $this->get('/');
-        $response->assertSeeText('No se encontraron resultados.');
-    }
+  public function testNoProducts(): void
+  {
+      $response = $this->get('/');
+      $response->assertSeeText('No se encontraron resultados.');
+  }
 
-    public function testStore(): void
-    {
-        $this
-          ->post(route('products'), [
-              'name' => 'Garden table',
-              'description' => 'Taula de jardi de kaoba',
-              'image' => 'url',
-              'price' => '200,00€'
-          ]);
-          // ->assertRedirect('/');
+  public function testStore(): void
+  {
+    $this
+      ->post(route('products'), [
+        'name' => 'Garden table',
+        'description' => 'Taula de jardi de kaoba',
+        'image' => 'url',
+        'price' => '200,00€'
+      ]);
+      // ->assertRedirect('/');
 
-        $this->assertDatabaseHas('products', [
-              'name' => 'Garden table',
-              'description' => 'Taula de jardi de kaoba',
-              'image' => 'url',
-              'price' => '200,00€'
-            ]);
-    }
+    $this->assertDatabaseHas('products', [
+        'name' => 'Garden table',
+        'description' => 'Taula de jardi de kaoba',
+        'image' => 'url',
+        'price' => '200,00€'
+      ]);
+  }
 }
