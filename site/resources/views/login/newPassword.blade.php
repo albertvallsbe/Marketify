357a2b216cc1bafe8e89f7cda1e7cc56640a8742
Albert Valls <avalls89@gmail.com>
2023-04-20 16:39:09 +0200

    Update newPassword view

--- site/resources/views/login/newPassword.blade.php
+++ site/resources/views/login/newPassword.blade.php
@@ -4,10 +4,11 @@
 
 @section('content')
     <section class="main-container">
-        <section class="card-style-medium remember-section remember-container">
-            <h1 class="card-style-medium_title remember-title">Have you forgotten the password?</h1>
-            <p class="card-style-medium_content remember-content">Have you forgotten the password? Don't worry. Enter your new password and we will update you new password.
-                <br><br>If you want to return press <a href={{route('login.index')}}>Login</a>
+        <section class="card-style-new-password remember-section remember-container">
+            <h1 class="card-style-new-password_title remember-title">Have you forgotten the password?</h1>
+            <p class="card-style-new-password_content remember-content">Have you forgotten the password? Don't worry. Enter your new password and we will update you new password.
+                <br><br>If you want to return press
+                <a href={{route('login.index')}}>Login</a>
             </p>
             <form class="form" method='POST'action="{{ route('login.rememberpassw') }}">
                 @csrf
