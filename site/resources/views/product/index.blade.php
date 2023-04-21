0bb71ff2683a47a8e890046d0620e4850a1860df
Albert Valls <avalls89@gmail.com>
2023-04-21 14:01:40 +0200

    Start add style to product detail view

--- site/resources/views/product/index.blade.php
+++ site/resources/views/product/index.blade.php
@@ -3,33 +3,33 @@
 @section('title', 'Marketify')
 
 @section('content')
-  <h1 class="main_title">Products</h1>
-  <div id="products" class="products">
-    @if ($products->count())
-      @foreach ($products as $key => $product)
-        <div class="card-style-mini product" id={{ $product->id }}>
-          <a class="card-style-mini_a" href="{{ route('product.show', $product->id) }}">
-            <div class="product__div_img">
-              <img class="product__img" src="{{ $product->image }}" />
+    <h1 class="main_title">Products</h1>
+    <div id="products" class="products">
+        @if ($products->count())
+        @foreach ($products as $key => $product)
+            <div class="card-style-mini product" id={{ $product->id }}>
+            <a class="card-style-mini_a" href="{{ route('product.show', $product->id) }}">
+                <div class="product__div_img">
+                <img class="product__img" src="{{ $product->image }}" />
+                </div>
+                <h4 class="card-style-mini_title product__name">{{ $product->name }}</h4>
+                <h4 class="card-style-mini_title product__name">{{ $product->tag }}</h4>
+                <h3 class="card-style-mini_title product__price">{{ $product->price }} €</h3>
+            </a>
+            <button class="btn-cart small-button" data-product-id="{{ $product->id }}">Add to cart</button>
             </div>
-            <h4 class="card-style-mini_title product__name">{{ $product->name }}</h4>
-            <h4 class="card-style-mini_title product__name">{{ $product->tag }}</h4>
-            <h3 class="card-style-mini_title product__price">{{ $product->price }} €</h3>
-          </a>
-          <button class="btn-cart small-button" data-product-id="{{ $product->id }}">Add to cart</button>
-        </div>
-      @endforeach
-    @else
-      @if (session('request_search') != null)
-        <h2>No results were found with '<?= session('request_search') ?>'.</h2>
-      @else
-        <h2>No results were found.</h2>
-      @endif
-    @endif
-  </div>
-  <div id="pagination">
-    {{-- {{ $products->appends(['search' => $search]) }} --}}
-    {{ $products->links('vendor.pagination.default') }}
-  </div>
-  <script type ="module" src="{{ asset('js/pages/product_index.js') }}"></script>
+        @endforeach
+        @else
+        @if (session('request_search') != null)
+            <h2>No results were found with '<?= session('request_search') ?>'.</h2>
+        @else
+            <h2>No results were found.</h2>
+        @endif
+        @endif
+    </div>
+    <div id="pagination">
+        {{-- {{ $products->appends(['search' => $search]) }} --}}
+        {{ $products->links('vendor.pagination.default') }}
+    </div>
+    <script type ="module" src="{{ asset('js/pages/product_index.js') }}"></script>
 @endsection
--- site/resources/views/product/show.blade.php
+++ site/resources/views/product/show.blade.php
@@ -3,10 +3,18 @@
 @section('title', 'Product')
 
 @section('content')
-  <img src="../../{{ $product->image }}" height="auto" width=250>
-    <h1>{{ $product->name }}</h1>
-    <p>Description: {{ $product->description }}</p>
-    <p>Price: {{ $product->price }}</p>
-    <button class="btn-cart" data-product-id="{{ $product->id }}">Add to cart</button>
+    <section class="main-productshow">
+        <section class="card-style-productshow-image"=>
+            <img src="../../{{ $product->image }}" height="auto" width=250>
+        </section>
+        <section class="card-style-productshow-details">
+            <h1>{{ $product->name }}</h1>
+            <p>Description: {{ $product->description }}</p>
+            <p>Price: {{ $product->price }}</p>
+            <button class="btn-cart" data-product-id="{{ $product->id }}">Add to cart</button>
+
+        </section>
+
+    </section>
     <script type ="module" src="{{ asset('js/pages/product_show.js') }}"></script>
 @endsection
