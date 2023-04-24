5e5a70c66974eb0a609a550944ad97bce051ff33
David <hernandez.larrea.david@alumnat.copernic.cat>
2023-04-24 17:07:25 +0200

    Improved categories

--- site/database/factories/CategoryFactory.php
+++ site/database/factories/CategoryFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -14,10 +15,36 @@ class CategoryFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition()
-    {
-        return [
-            'name' => $this->faker->unique()->sentence(1),
-        ];
-    }
-}
+
+     protected $model = Category::class;
+
+     public function definition()
+     {
+         $categories = [
+             'Electronics',
+             'Clothing',
+             'Beauty',
+             'Home & Garden',
+             'Toys & Games',
+             'Sports & Outdoors',
+             'Books',
+             'Jewelry',
+             'Health & Wellness',
+             'Baby & Kids',
+             'Food & Beverage',
+             'Office Supplies',
+             'Automotive',
+             'Pet Supplies',
+             'Music & Movies',
+             'Travel',
+             'Art & Crafts',
+             'Party Supplies',
+             'Musical Instruments',
+             'Industrial & Scientific'
+         ];
+ 
+         return [
+            'name' => $this->faker->unique()->randomElement($categories),
+         ];
+     }
+ }
