3b3f5e2e8ac9cdf0f223d950ceb132f364e019f0
oscaarramirez23 <oscarferram@gmial.com>
2023-05-03 15:54:43 +0200

    button View More

--- site/resources/views/landing/index.blade.php
+++ site/resources/views/landing/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <section class="landing-container">
-        <a href="{{ route('products.index') }}">
+        <a href="{{ route('product.index') }}">
             <div class="container-slogan">
                 <img class="logo" src="{{ asset('images/marketify_logo.png') }}">
                 <h1 class="title-slogan">The best products at the best price!</h1>
@@ -18,30 +18,34 @@
                 <h2 class="title-product">producto</h2>
             </div>
         </div>
-            @foreach ($activeTags as $tag)
-                <div class="products-container">
-                    <a class="card-style-home_a" href="{{ route('product.filter', $tag['name']->id) }}">
-                        <h2 class="products-title">{{ $tag['name']->name }}</h2>
-                    </a>
-                </div>
-                <div class="products-section">
-                    @foreach ($tag['products'] as $tagProducts)
-                        <div class="card-style-home product" id={{ $tagProducts['id'] }}>
-                            <a class="card-style-home_a" href="{{ route('product.show', $tagProducts['id']) }}">
-                                {{-- </div>
+        @foreach ($activeTags as $tag)
+            <div class="products-container">
+                <a class="card-style-home_a" href="{{ route('product.filter', $tag['name']->id) }}">
+                    <h2 class="products-title">{{ $tag['name']->name }}</h2>
+                </a>
+            </div>
+            <div class="products-section">
+                @foreach ($tag['products'] as $tagProducts)
+                    <div class="card-style-home product" id={{ $tagProducts['id'] }}>
+                        <a class="card-style-home_a" href="{{ route('product.show', $tagProducts['id']) }}">
+                            {{-- </div>
                         <div class="product__div_img"> --}}
-                                <img class="product-img" src="{{ $tagProducts['image'] }}" />
-                                <h4 class="card-style-home_title product__name">{{ $tagProducts['name'] }}</h4>
-                                <h4 class="card-style-home_title product__name">{{ $tagProducts['tag'] }}</h4>
-                                <h3 class="card-style-home_title product__price">{{ $tagProducts['price'] }} €</h3>
-                            </a>
-                            {{-- <button class="btn-cart small-button" data-product-id="{{ $tagProducts['id'] }}">Add to
+                            <img class="product-img" src="{{ $tagProducts['image'] }}" />
+                            <h4 class="card-style-home_title product__name">{{ $tagProducts['name'] }}</h4>
+                            <h4 class="card-style-home_title product__name">{{ $tagProducts['tag'] }}</h4>
+                            <h3 class="card-style-home_title product__price">{{ $tagProducts['price'] }} €</h3>
+                        </a>
+                        {{-- <button class="btn-cart small-button" data-product-id="{{ $tagProducts['id'] }}">Add to
                             cart</button> --}}
-                        </div>
-                    @endforeach
-                </div>
-            @endforeach
-
+                    </div>
+                @endforeach
+            </div>
+        @endforeach
+        <div class="section-btn">
+            <a href="{{ route('product.index') }}">
+                <button class="btn-see-more">VIEW MORE</button>
+            </a>
+        </div>
     </section>
 
 @endsection
--- site/routes/web.php
+++ site/routes/web.php
@@ -25,7 +25,7 @@ Route::get('/search', [ProductController::class,'index'])->name('product.index')
 Route::get('/search/{id}', [ProductController::class, 'filterCategory'])->name('product.filter');
 // Route::get('/products', [RegisterController::class, 'index'])->name('products.index');
 
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+// Route::get('/products', [ProductController::class, 'index'])->name('product.index');
 Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
 // Route::post('/product', [ProductController::class, 'store'])->name('products');
 Route::post('/product', [ProductController::class, 'store'])->name('product.store');
