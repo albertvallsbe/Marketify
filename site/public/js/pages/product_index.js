05992b7e3cb62a0506d8194712cef2a169f4a1d2
Albert Valls <avalls89@gmail.com>
2023-05-06 12:57:57 +0200

    Update index

--- site/public/js/pages/product_index.js
+++ site/public/js/pages/product_index.js
@@ -1,7 +1,7 @@
 import "../app.js";
-// import words from "../dictionaryWords.js";
+import words from "../dictionaryWords.js";
 
-// const buttonLabel = document.getElementById("button-prueba");
-// console.log(buttonLabel);
-// console.log(words);
-// buttonLabel.innerText = words.buttons.add;
+const buttonLabel = document.getElementById("button-prueba");
+console.log(buttonLabel);
+console.log(words);
+buttonLabel.innerText = words.buttons.add;
